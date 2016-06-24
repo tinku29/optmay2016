f6fa162b598a7d1e4ff9d4d01e815a4c4b94ff3a
Kevin Santhosh <Kevin Santhosh@Mark.fios-router.home>
2016-06-24 19:12:10 -0400

    chapter 2 ex

---
+++ opt-may-2016/kevin/src1/SumSquare.java
@@ -0,0 +1,12 @@
+package src1;
+
+public class SumSquare {
+	public static void main(String args[]){
+		int sum=0;
+		for(int i=1;i<=5;i++){
+			sum=sum+i*i;
+		}
+		System.out.println("Sum of the squares of the frst 5 numbers are"+sum);
+	}
+
+}
