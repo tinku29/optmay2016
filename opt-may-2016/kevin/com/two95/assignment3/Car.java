df4aa40645561c6a35b41344fd633bc223a695d0
Kevin Santhosh <Kevin Santhosh@Mark.fios-router.home>
2016-06-24 18:37:04 -0400

    The third assignments

---
+++ opt-may-2016/kevin/com/two95/assignment3/Car.java
@@ -0,0 +1,19 @@
+package com.two95.assignment3;
+
+public class Car {
+	private String model;
+	private int milage;
+    public void setModel(String Model){
+    	this.model=Model;
+    }
+    public String getModel(){
+		return model;
+	}
+	public void setMilage(int Milage){
+		this.milage=Milage;
+	}
+	public int getMilage(){
+		return milage;
+	}
+
+}
---
+++ opt-may-2016/kevin/com/two95/assignment3/Trainee.java
@@ -0,0 +1,33 @@
+package com.two95.assignment3;
+
+public class Trainee {
+	private String name;
+	private int age;
+	private String course;
+	private int id;
+	public void setName(String Name){
+		this.name=Name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setAge(int Age){
+		this.age=Age;
+	}
+	public int getAge(){
+		return age;
+	}
+	public void setCourse(String Course){
+		this.course=Course;
+	}
+	public String getCourse(){
+		return course;
+	}
+	public void setId(int Id){
+		this.id=Id;
+	}
+	public int getId(){
+		return id;
+	}
+
+}
