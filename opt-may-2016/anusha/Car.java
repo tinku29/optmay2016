d93519f41b5798fac4ce0c4a898179620ef089c1
vinay <vinay@anusharao.home>
2016-06-25 19:52:41 -0400

    intial commit

---
+++ opt-may-2016/anusha/Car.java
@@ -0,0 +1,35 @@
+
+
+public class Car {
+	private String Color;
+	private String Model;
+	private String Company;
+
+         public String getColor(){
+			return Color;
+		}
+		public String getModel(){
+			return Model;
+		}
+		public String getCompany(){
+			return Company;
+		}
+		public void setColor(String color){
+			 this.Color = color;
+		}
+		public void setModel(String Model){
+			this.Model = Model;
+		}
+		public void setCompany(String Company){
+			this.Company = Company;
+		}
+		
+	
+
+	 {
+	
+
+	}
+
+}
+
