4872a4f72ea6e63a17e118dc0e09948a2d36676a
Bhavin <Bhavin@BMP>
2016-06-25 19:52:24 -0400

    sum Square

---
+++ opt-may-2016/bhavin/com/bhavin/training/SumSqure.java
@@ -0,0 +1,15 @@
+package com.bhavin.training;
+
+public class SumSqure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long sum = 0;
+		for (int i=1; i<=30; i++) {
+		sum = sum + i * i;
+		System.out.println("The sum of squares of first 30 numbers is "  + sum );
+		}
+		System.out.println("The sum of squares of first 30 numbers is "  + sum );
+	}
+
+}
