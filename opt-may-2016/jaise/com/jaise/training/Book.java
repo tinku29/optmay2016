d68576729dff38e9efb6caae6ff720f7d1973761
JAISE <JAISE@DESKTOP-D6PPOFT>
2016-06-25 19:29:14 -0400

    comiting

---
+++ opt-may-2016/jaise/com/jaise/training/Book.java
@@ -0,0 +1,15 @@
+package com.jaise.training;
+
+public class Book {
+	 private double bookPrice;
+
+	public void setPrice(double price) {
+		bookPrice = price;
+	}
+	
+	public double getPrice(){
+		
+		return bookPrice;
+	}
+}
+
