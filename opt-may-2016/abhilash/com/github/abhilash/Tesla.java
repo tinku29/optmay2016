097d95664c7b8ba815b738e51603348cb2f5b8e6
Abhilash <Abhilash@Abhilash-PC>
2016-06-26 03:46:18 +0530

    assignment

---
+++ opt-may-2016/abhilash/com/github/abhilash/Tesla.java
@@ -0,0 +1,18 @@
+package com.github.abhilash;
+
+public class Tesla extends Car{
+	
+	Tesla(){
+		super("Tesla",45,60000);
+	}
+
+	public static void main(String[] args) {
+ 
+		Tesla t = new Tesla();
+		t.printDetails();
+		t.bodyType("suv");
+		t.fuel("Electric Battery "); 
+		
+	}
+
+}
