2c878e5d0d0735834b40e73e31f4ff75d35b2aee
Sooraj Kesavadas <sooraj139@gmail.com>
2016-06-30 14:34:22 -0400

    Update ToyotaDealer.java

--- opt-may-2016/Sooraj/com/training/polymorphism/ToyotaDealer.java
+++ opt-may-2016/Sooraj/com/training/polymorphism/ToyotaDealer.java
@@ -2,8 +2,10 @@ package com.training.polymorphism;
 
 public class ToyotaDealer {
 	public void implement(Vehicle T){
+		System.out.println("Welcome to Toyota dealership");
+		System.out.println("The cars have the following specifications");
 		T.goFast();
-	    T.stop();
+	        T.stop();
 		T.reverse();
 		
 	}
