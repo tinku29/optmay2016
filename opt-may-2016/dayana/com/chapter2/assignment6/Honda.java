ac61fd720fc3ee9c2be72f25546ec84d6e9ea58f
dmallass <dmallass@hawk.iit.edu>
2016-06-30 17:06:17 -0500

    Assignment6

---
+++ opt-may-2016/dayana/com/chapter2/assignment6/Honda.java
@@ -0,0 +1,17 @@
+package com.chapter2.assignment6;
+
+public class Honda extends Car{
+
+	public void register()
+	{
+		System.out.println("All honda cars are registered in DMV");
+	}
+	public void carWash()
+	{
+		System.out.println("wash honda car for free");
+	}
+	public void sell()
+	{
+		System.out.println("Sell a Honda");
+	}
+}
---
+++ opt-may-2016/dayana/com/chapter2/assignment6/Vehicle.java
@@ -0,0 +1,17 @@
+package com.chapter2.assignment6;
+
+public class Vehicle {
+	
+//	public void addFuel()
+//	{
+//		System.out.println("fuel level of vehicle is low. Fill gas");
+//	}
+	public void sell()
+	{
+		System.out.println("Sell a vehicle");
+	}
+	public void register()
+	{
+		System.out.println("All Vehicles are registered in DMV");
+	}
+}
