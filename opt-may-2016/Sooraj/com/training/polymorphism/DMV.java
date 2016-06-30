0219fb9885b1c4c585039399f88c7a0a3302985a
Sooraj Kesavadas <sooraj139@gmail.com>
2016-06-30 13:20:14 -0400

    Update DMV.java

--- opt-may-2016/Sooraj/com/training/polymorphism/DMV.java
+++ opt-may-2016/Sooraj/com/training/polymorphism/DMV.java
@@ -1,10 +1,14 @@
 package com.training.polymorphism;
 
 public class DMV {
-	public void Truck1(Vehicle v){
+	public void registerVehicles(Vehicle v){
+		System.out.println("Starting the registration");
+		System.out.println("---------------------------");
 		v.goFast();
 		v.stop();
 		v.reverse();
+		System.out.println("End of registration");
+		System.out.println("---------------------------");
 		
 	}
 //
