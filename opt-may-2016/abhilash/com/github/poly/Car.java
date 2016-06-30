0dbcf442597e169739c4cef42a7639217fa1dea2
Abhilash <Abhilash@Abhilash-PC>
2016-06-30 08:05:21 +0530

    Polymorphism assignment

---
+++ opt-may-2016/abhilash/com/github/poly/Car.java
@@ -0,0 +1,14 @@
+package com.github.poly;
+
+public class Car extends Vehicle {
+
+	public void carwash(){
+		System.out.println("Car wash selected");
+	}
+	
+	public void dmv(){
+		System.out.println("Car selected to Register at DMV");
+		}
+	
+	
+}
