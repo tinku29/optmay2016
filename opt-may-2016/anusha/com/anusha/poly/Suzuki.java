aae289922f19caa158255efebe6478e9e22b132e
vinay <vinay@anusharao>
2016-07-04 22:50:33 -0400

    poly

---
+++ opt-may-2016/anusha/com/anusha/poly/Suzuki.java
@@ -0,0 +1,20 @@
+package com.anusha.poly;
+
+public class Suzuki extends Car {
+
+	@Override
+	public void sell() {
+		System.out.println("sell all suzuki cars");
+	}
+
+	@Override
+	public void wash() {
+		System.out.println("wash all suzuki cars");
+	}
+
+	@Override
+	public void register() {
+		System.out.println("register all suzuki cars");
+	}
+
+}
