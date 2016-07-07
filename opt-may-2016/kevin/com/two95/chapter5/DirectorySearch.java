8aff825ef10ac2d47b1ddddeab64ed9cea3a60c4
Kevin Santhosh <Kevin Santhosh@Mark.fios-router.home>
2016-07-07 18:24:35 -0400

    more examples in chapter7

---
+++ opt-may-2016/kevin/com/two95/chapter5/DirectorySearch.java
@@ -0,0 +1,7 @@
+package com.two95.chapter5;
+
+public interface DirectorySearch {
+	public void searchNovel();
+	public void searchLdap();
+
+}
---
+++ opt-may-2016/kevin/com/two95/chapter5/SuperiorSearchTool.java
@@ -0,0 +1,15 @@
+package com.two95.chapter5;
+
+public class SuperiorSearchTool extends PremierSearchTool{
+	    public void searchNovel(){
+		System.out.println("Performing enhanced Novel Search");
+		System.out.println("Found 20 Novel results");
+		}
+		public void searchLdap() {
+		System.out.println("Performing enhanced LDAP Search");
+		System.out.println("Found 15 results in LDAP");
+		}
+		public void searchLibraries() {
+			System.out.println("Found 9 results in Libraries");
+			}
+		}
