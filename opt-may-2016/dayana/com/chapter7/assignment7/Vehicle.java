bf09db131b72c1f3f068b39cb9e154abc2b4bccc
dmallass <dmallass@hawk.iit.edu>
2016-07-09 10:09:36 -0500

    Generics

---
+++ opt-may-2016/dayana/com/chapter7/assignment7/Vehicle.java
@@ -0,0 +1,25 @@
+package com.chapter7.assignment7;
+
+public class Vehicle {
+	private int make;
+	private String model;
+	public Vehicle(int make, String model) {
+		super();
+		this.make = make;
+		this.model = model;
+	}
+	public int getMake() {
+		return make;
+	}
+	public void setMake(int make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+}
