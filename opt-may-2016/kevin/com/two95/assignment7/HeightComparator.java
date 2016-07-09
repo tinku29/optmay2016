fa18826eb77d0af53e3ef8add7ac87dfc89a5d19
Kevin Santhosh <Kevin Santhosh@Mark.fios-router.home>
2016-07-09 13:24:30 -0400

    some corrections

--- opt-may-2016/kevin/com/two95/assignment7/HeightComparator.java
+++ opt-may-2016/kevin/com/two95/assignment7/HeightComparator.java
@@ -4,7 +4,7 @@ public class HeightComparator implements Comparator {
 	public int compare(Object o1,Object o2){
 		Student s1=(Student) o1;
 		Student s2=(Student) o2;
-		int index=s1.getLastName().compareTo(s2.getLastName());
+		int index=s1.getHeight().compareTo(s2.getHeight());
 		return index;
 	}
 }
--- opt-may-2016/kevin/com/two95/assignment7/StudentSortTest1.java
+++ opt-may-2016/kevin/com/two95/assignment7/StudentSortTest1.java
@@ -8,29 +8,45 @@ public class StudentSortTest1 {
 		TreeSet set2=new TreeSet(new AgeComparator());
 		TreeSet set3=new TreeSet(new HeightComparator());
 		set.add(new Student("Anusha","Rao","24","172.5"));
-		set.add(new Student("Abilash","Madavaram","24","180.0"));
-		set.add(new Student("Krishna","Kalyanam","24","179.2"));
+		set.add(new Student("Abilash","Madavaram","23","180.0"));
+		set.add(new Student("Krishna","Kalyanam","25","179.2"));
 		set.add(new Student("Kevin","Santhosh","21","175.5"));
-		set.add(new Student("Dayana","Sunny","23","173.2"));
+		set.add(new Student("Dayana","Sunny","22","173.2"));
+		set1.add(new Student("Anusha","Rao","24","172.5"));
+		set1.add(new Student("Abilash","Madavaram","23","180.0"));
+		set1.add(new Student("Krishna","Kalyanam","25","179.2"));
+		set1.add(new Student("Kevin","Santhosh","21","175.5"));
+		set1.add(new Student("Dayana","Sunny","22","173.2"));
+		set2.add(new Student("Anusha","Rao","24","172.5"));
+		set2.add(new Student("Abilash","Madavaram","23","180.0"));
+		set2.add(new Student("Krishna","Kalyanam","25","179.2"));
+		set2.add(new Student("Kevin","Santhosh","21","175.5"));
+		set2.add(new Student("Dayana","Sunny","22","173.2"));
+		set3.add(new Student("Anusha","Rao","24","172.5"));
+		set3.add(new Student("Abilash","Madavaram","23","180.0"));
+		set3.add(new Student("Krishna","Kalyanam","25","179.2"));
+		set3.add(new Student("Kevin","Santhosh","21","175.5"));
+		set3.add(new Student("Dayana","Sunny","22","173.2"));
+		
 		Iterator itr=set.iterator();
 		while(itr.hasNext()){
 			Student student=(Student)itr.next();
 			System.out.println(student.getLastName()+" "+student.getFirstName()+" "+student.getAge()+" "+student.getHeight()+" ");
 		}
 		System.out.println("**************************************");
-		Iterator itr1=set.iterator();
+		Iterator itr1=set1.iterator();
 		while(itr1.hasNext()){
 			Student student=(Student)itr1.next();
 			System.out.println(student.getFirstName()+" "+student.getLastName()+" "+student.getAge()+" "+student.getHeight()+" ");
 		}
 		System.out.println("***************************************");
-		Iterator itr2=set.iterator();
+		Iterator itr2=set2.iterator();
 		while(itr2.hasNext()){
 			Student student=(Student)itr2.next();
 			System.out.println(student.getFirstName()+" "+student.getLastName()+" "+student.getAge()+" "+student.getHeight()+" ");
 		}
 		System.out.println("****************************************");
-		Iterator itr3=set.iterator();
+		Iterator itr3=set3.iterator();
 		while(itr3.hasNext()){
 			Student student=(Student)itr3.next();
 			System.out.println(student.getFirstName()+" "+student.getLastName()+" "+student.getAge()+" "+student.getHeight()+" ");
