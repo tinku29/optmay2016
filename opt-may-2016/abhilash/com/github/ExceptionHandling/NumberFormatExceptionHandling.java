db00f6f5d69a6e0e47ea3b8ccc8f05025c94aeed
tinku29 <madhavaram.tinku@gmail.com>
2016-07-17 02:52:00 +0530

    Exception Handling Assignment

---
+++ opt-may-2016/abhilash/com/github/ExceptionHandling/NumberFormatExceptionHandling.java
@@ -0,0 +1,34 @@
+package com.github.ExceptionHandling;
+
+import java.util.Scanner;
+
+public class NumberFormatExceptionHandling {
+
+	public static void main(String[] args) {
+		 
+		String a = "a";
+		String b = "m";
+		
+		
+		/*int aa = Integer.parseInt(a);
+		int bb = Integer.parseInt(b);
+	
+		System.out.println("The converted value is " + aa + bb);*/
+		
+		//Scanner sc = new Scanner(System.in);
+		try{
+			/*a = Integer.parseInt(sc.next());
+			b = Integer.parseInt(sc.next());*/
+			
+			int sum = Integer.parseInt(a + b);
+			
+			System.out.println(sum);
+		}catch(NumberFormatException n){
+			System.out.println(n.getMessage() + " is Number Format Exception ");
+		}finally {
+			System.out.println("Printing Finally ");
+		}
+
+	}
+
+}
